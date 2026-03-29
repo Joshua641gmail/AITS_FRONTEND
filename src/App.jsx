3d7ddfe27e2dab861b237ed0d87e1b70d6068dfa
SSERUNKUUMA JOSHUA <sserunkuumajoshua641@gmail.com>
2026-03-29 23:55:57 +0300

    chore: enable routing to the login

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 
-function App() {
+/*function App() {
   return (
     <div className="text-blue-600 text-2xl font-bold p-8">
       AITS Frontend is working!
     </div>
+  )
+}
+
+export default App*/
+import { Routes, Route, Navigate } from 'react-router-dom'
+import Login from './pages/Login'
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigate to="/login" />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
   )
 }
 
